76d9f38d36fd7685bc5ccb35c6b3e86932b5e7f2
Lillian <weijingwen35@foxmail.com>
2020-08-07 18:10:43 +0800

    style config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/index.scss';
 import Hello from './component/Hello';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
