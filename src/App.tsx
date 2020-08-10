954375c2cb3813e2b53331e23781985a0f64b5e5
Lillian <weijingwen35@foxmail.com>
2020-08-10 21:56:51 +0800

    alert component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles/index.scss';
 import Button, { ButtonType, ButtonSize } from './component/Button/button';
+import { Alert, AlertType } from './component/Alert/alert';
 
 function App() {
   return (
@@ -16,6 +17,13 @@ function App() {
         <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Danger</Button>
         <Button btnType={ButtonType.Warning} size={ButtonSize.Small}>Warning</Button>
         <Button btnType={ButtonType.Link} href="http://www.baidu.com" children="Baidu" />
+      </div>
+      <div>
+        <h1>Alert</h1>
+        <Alert alterType={AlertType.Success} closable={false} description='success without close' />
+        <Alert alterType={AlertType.Info} title='this is Info alert' />
+        <Alert alterType={AlertType.Warning} title='Waring Waring Waring!' />
+        <Alert alterType={AlertType.Danger} title='Danger' description='something is wrong' />
       </div>
     </div>
   );
---
+++ src/component/Alert/alert.tsx
@@ -0,0 +1,79 @@
+import React, { ReactNode, FC, useState, Fragment } from 'react';
+import classNames from 'classnames';
+import { Transition } from '../Transition/transition'
+import { CSSTransition } from 'react-transition-group';
+
+export enum AlertType {
+    Success = 'success',
+    Info = 'info',
+    Danger = 'danger',
+    Warning = 'warning'
+}
+
+interface BaseAlertProps {
+    title?: string;
+    description?: string;
+    className?: string;
+    alterType?: AlertType;
+    onClose?: () => void;  // 关闭alert时触发的事件
+    closable?: boolean;     // 是否显示“关闭”
+}
+
+export const Alert: FC<BaseAlertProps> = (props) => {
+
+    const [hide, setHide] = useState(false);
+    const [showButton, setShowButton] = useState(false);
+
+    const {
+        title,
+        description,
+        className, // 用户自定义的className
+        alterType,
+        onClose,
+        closable
+    } = props;
+
+    const classes = classNames('alt', className, {
+        [`alt-${alterType}`]: alterType
+    })
+
+    const titleClass = classNames( {
+        'alt-description':!description,
+        'alt-title': description
+    })
+
+    const handleClose = (e: React.MouseEvent) => {
+        if (onClose) {
+            onClose()
+        } else {
+            setHide(true)
+        }
+    }
+
+    return (
+        <Fragment>
+            {showButton && <span className="alt-show-btn" onClick={() => { setHide(false) }}>Show Alert</span>}
+            {console.log(showButton)}
+            <CSSTransition
+                in={!hide}
+                timeout={400}
+                unmountOnExit
+                onEnter={() => { setShowButton(false) }}
+                onExited={() => setShowButton(true)}
+            >
+                <div className={classes} >
+                    <div className="alt-message">
+                        <span className={titleClass}> {title} </span>
+                        {description&&<span className="alt-description"> {description} </span>}
+                    </div>
+                    {closable && <button className="alt-close-button" onClick={handleClose}> + </button>}
+                </div>
+            </CSSTransition>
+        </Fragment>
+    )
+}
+
+Alert.defaultProps = {
+    alterType: AlertType.Info,
+    closable: true,
+}
