61cd46f15618d3b31812b159929840eccc04b6b9
Lillian <weijingwen35@foxmail.com>
2020-09-10 12:12:08 +0800

    fetch suggestion list

--- src/component/AutoComplete/autoComplete.stories.tsx
+++ src/component/AutoComplete/autoComplete.stories.tsx
@@ -1,22 +1,46 @@
 import React from 'react';
 import { Meta } from '@storybook/react/types-6-0';
 import { action } from '@storybook/addon-actions'
-import { AutoComplete } from './autoComplete';
+import { AutoComplete, DataSourceType } from './autoComplete';
 
 export default {
     title: 'Component/AutoComplete',
     component: AutoComplete,
 } as Meta;
 
-const happy = ['sss', 'rrr', 'sdfa', 'vavwezs']
-const handleFetch = (query: string, list: string[]) => list.filter(name => name.includes(query))
-const option = (item: string) => (
-    <span>history: {item}</span>
-)
+// const happy = ['sss', 'rrr', 'sdfa', 'vavwezs']
+// const handleFetch = (query: string, list: string[]) => list.filter(name => name.includes(query))
+interface GithubUserProps {
+    login: string;
+    url: string;
+}
+const handleFetch = (query: string) => {
+    return fetch(`https://api.github.com/search/users?q=${query}`)
+        .then(
+            res => {
+                const clone = res.clone();
+                return clone.json()
+            }
+        ).then(
+            ({ items }) => {
+                console.log(items)
+                const f = items.slice(0, 10).map((item: { login: any; }) => ({ value: item.login, ...item }))
+                return f;
+            }
+        )
+}
+const option = (item: DataSourceType) => {
+    const items = item as DataSourceType<GithubUserProps>
+    return (
+        <>
+            <h6>name:{items.login}</h6>
+            <p>url:{items.url}</p>
+        </>
+    )
+}
 
 export const Default = () => (
     <AutoComplete
-        history={happy}
         fetchSuggestion={handleFetch}
         onSelect={action('selected')}
         renderOption={option}
--- src/component/AutoComplete/autoComplete.tsx
+++ src/component/AutoComplete/autoComplete.tsx
@@ -1,54 +1,67 @@
 import React, { FC, useState, ChangeEvent, ReactElement } from 'react';
 import { InputProps, Input } from '../Input/input';
 
+interface DataSourceObject {
+    value: string
+}
+export type DataSourceType<T = {}> = T & DataSourceObject
 interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
-    fetchSuggestion: (value: string, list: string[]) => string[];
-    onSelect?: (item: string) => void;
-    renderOption?: (item: string) => ReactElement;
-    history?: string[];
+    fetchSuggestion: (value: string) => DataSourceType[]|Promise<DataSourceType[]>;
+    onSelect?: (item: DataSourceType) => void;
+    renderOption?: (item: DataSourceType) => ReactElement;
+    fetchFetch?: any
+    // fetchSuggestion: (value: string, list: string[]) => string[];
+    // history?: string[];
 }
 
 export const AutoComplete: FC<AutoCompleteProps> = (props) => {
     const {
         value,
-        history = [],
+        // history = [],
         fetchSuggestion,
         onSelect,
         renderOption,
         ...restProps
     } = props;
 
-    const [suggestion, setSuggestion] = useState<string[]>([])
+    const [suggestion, setSuggestion] = useState<DataSourceType[]>([])
     const [inputValue, setInputValue] = useState(value)
-    const [inputHistory, setHistory] = useState<string[]>(history)
+    // const [inputHistory, setHistory] = useState<string[]>(history)
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         let value = e.target.value.trim()
         setInputValue(value)
         if (value) {
-            const results = fetchSuggestion(value, inputHistory)
-            setSuggestion([...results, value])
+            // const results = fetchSuggestion(value, inputHistory)
+            const results = fetchSuggestion(value)
+            if (results instanceof Promise) {
+                results.then(data => {
+                    setSuggestion(data)
+                })
+            } else {
+                setSuggestion(results)
+            }
         } else {
             setSuggestion([])
         }
     }
 
-    const handleSelect = (item: string) => {
-        setInputValue(item)
+    const handleSelect = (item: DataSourceType) => {
+        setInputValue(item.value)
         setSuggestion([])
         if (onSelect) {
             onSelect(item)
         }
     }
 
-    const handleBlur = (item: any) => {
-        console.log('blur--', item)
-        if (item)
-            setHistory([...inputHistory, item])
-    }
+    // const handleBlur = (item: any) => {
+    //     console.log('blur--', item)
+    //     if (item)
+    //         setHistory([...inputHistory, item])
+    // }
 
-    const renderTemplate = (item: string) => {
-        return renderOption ? renderOption(item) : item
+    const renderTemplate = (item: DataSourceType) => {
+        return renderOption ? renderOption(item) : item.value
     }
 
     const generateDropdown = () => {
@@ -68,7 +81,7 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
             <Input
                 value={inputValue}
                 onChange={handleChange}
-                onBlur={() => handleBlur(inputValue)}
+                // onBlur={() => handleBlur(inputValue)}
                 {...restProps}
             />
             {suggestion.length > 0 && generateDropdown()}
