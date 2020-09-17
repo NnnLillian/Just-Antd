9249ea2ae5bb26b4f9c4311e9b983f7bb09a3178
Lillian <weijingwen35@foxmail.com>
2020-09-17 17:58:32 +0800

    autoComplete & input storybook

--- src/component/AutoComplete/autoComplete.stories.tsx
+++ src/component/AutoComplete/autoComplete.stories.tsx
@@ -5,12 +5,109 @@ import { AutoComplete, DataSourceType } from './autoComplete';
 
 
 export default {
-    title: 'Component/AutoComplete',
-    component: AutoComplete,
+    title: 'Component/AutoComplete 自动补全输入框',
+    // component: AutoComplete,
+    argTypes: {
+        disable: {
+            description: '输入框失效状态',
+            table: {
+                type: { summary: 'boolean' },
+                defaultValue: { summary: 'true' }
+            },
+            control: {
+                type: 'boolean'
+            }
+        },
+        size: {
+            description: '设置输入框大小',
+            table: {
+                type: { summary: 'lg|sm' },
+                defaultValue: { summary: 'sm' }
+            },
+            control: {
+                type: 'select',
+                options: ['lg', 'sm']
+            }
+        },
+        fetchSuggestion: {
+            description: '获取提示数据源方法',
+            type: {
+                required: true,
+            },
+            table: {
+                type: {
+                    summary: 'function(query:string)',
+                    detail: '(query: string) => DataSourceType[] | Promise<DataSourceType[]>;'
+                },
+            },
+            control: {
+                type: 'null'
+            }
+        },
+        onSelect: {
+            description: '被选中时调用，参数为选中项',
+            table: {
+                type: {
+                    summary: 'function(item: DataSourceType)',
+                    detail: '(item: DataSourceType) => void;'
+                },
+            }
+        },
+        renderOption: {
+            description: '自定义提示样式',
+            table: {
+                type: {
+                    summary: 'function(item: DataSourceType)',
+                    detail: '(item: DataSourceType) => ReactElement;'
+                },
+            }
+        }
+    },
+    parameters: {
+        docs: {
+            description: { component: '**输入框自动完成功能**' },
+            source: {
+                code:
+                    `
+import { AutoComplete, DataSourceType } from './component/AutoComplete';
+
+interface GithubUserProps {
+    login: string;
+    url: string;
+}
+
+const fetchFetch = (query: string) => {
+    return fetch(\`https://api.github.com/search/users?q=\$\{query\}\`)
+        .then(
+            res => res.clone().json()
+        ).then(
+            ({ items }) => (items.slice(0, 10).map((item: { login: any; }) => ({ value: item.login, ...item }))
+        )
+    }
+
+const renderOption = (item: DataSourceType) => {
+    const items = item as DataSourceType<GithubUserProps>
+    return (
+        <>
+            <h6>name:{items.login}</h6>
+            <p>url:{items.url}</p>
+        </>
+    )
+}
+
+() => (
+    <AutoComplete
+        fetchSuggestion={fetchFetch}
+        renderOption={renderOption}
+    />
+)   
+`
+            }
+        }
+    },
+    decorators: [(Story: any) => <div style={{ width: "600px" }}><Story /></div>]
 } as Meta;
 
-// const happy = ['sss', 'rrr', 'sdfa', 'vavwezs']
-// const handleFetch = (query: string, list: string[]) => list.filter(name => name.includes(query))
 interface GithubUserProps {
     login: string;
     url: string;
@@ -40,10 +137,13 @@ const option = (item: DataSourceType) => {
     )
 }
 
-export const Default = () => (
+const autoCompleteInput = (args: any) => (
     <AutoComplete
         fetchSuggestion={handleFetch}
         onSelect={action('selected')}
         renderOption={option}
+        {...args}
     />
 )
+
+export const Default = autoCompleteInput.bind({})
--- src/component/Input/input.stories.tsx
+++ src/component/Input/input.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Meta } from '@storybook/react/types-6-0';
-import { Input } from './input';
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { Input, InputProps } from './input';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 
@@ -12,27 +12,84 @@ export default {
     parameters: {
         actions: {
             handles: ['change']
+        },
+        docs: {
+            description: { component: '**通过鼠标或键盘输入内容，基础表单域的包装**' }
         }
     },
     // 装饰文件
-    decorators: [(Story: any) => <div style={{ width: "300px", margin: "auto" }}><Story /></div>],
+    decorators: [(Story: any) => <div style={{ width: "300px" }}><Story /></div>],
 } as Meta;
 
-export const Default = () => (
-    <Input />
+const InputStory: Story<InputProps> = (args) => (
+    <Input {...args} />
 )
-Default.story = {
-    name: 'default 输入框',
+
+export const Default = InputStory.bind({})
+Default.storyName = "Default 输入框"
+Default.argTypes = {
+    placeholder: {
+        control: 'text',
+    },
+    icon: {
+        control: 'text'
+    },
+    className: {
+        control: 'text',
+        table: { defaultValue: { summary: 'input' } }
+    },
+    disabled: {
+        table: { defaultValue: { summary: 'false' } }
+    },
+    prepend: {
+        control: 'text',
+        table: { type: { summary: 'string' } }
+    },
+    append: {
+        control: 'text',
+        table: { type: { summary: 'string' } }
+    }
+}
+Default.args = {
+    placeholder: 'default'
+}
+Default.parameters = {
+    backgrounds: {
+        default: 'primary',
+        values: [
+            { name: 'primary', value: '#806d9e' },
+            { name: 'light', value: '#fff' },
+        ],
+    },
 }
 
-export const disableInput = () => <Input placeholder="disabled input" disabled />
-disableInput.story = {
-    name: '被禁用的 Input',
+// export const disableInput = () => <Input placeholder="disabled input" disabled />
+export const disableInput = InputStory.bind({})
+// disableInput.story = {
+//     name: '被禁用的 Input',
+// }
+disableInput.parameters = {
+    docs: {
+        source: {
+            code: "import Input from './input' \n \n <Input disabled/>"
+        }
+    },
+    controls: {
+        hideNoControlsWarning: true,
+    }
 }
 
 export const iconInput = () => <Input icon="search" placeholder="input with icon" />
 iconInput.story = {
     name: '带图标的 Input',
+}
+iconInput.parameters = {
+    docs: {
+        source: {
+            code: "import Input from './input' \n \n <Input icon='search' placeholder='input with icon' />"
+        }
+    },
+    controls: { hideNoControlsWarning: true }
 }
 
 export const sizeInput = () => (
@@ -42,15 +99,31 @@ export const sizeInput = () => (
     </>
 )
 sizeInput.story = {
-    name: '不同尺寸的 Input'
+    name: '不同尺寸的 Input',
+    parameters: {
+        docs: {
+            source: {
+                code: "import Input from './input' \n \n <Input defaultValue='large size' size='lg' /> \n <Input placeholder='small size' size='sm' />"
+            }
+        },
+        controls: { hideNoControlsWarning: true }
+    }
 }
 
 export const pandInput = () => (
-    <>
+    <div>
         <Input defaultValue="prepend text" prepend="https//" />
         <Input defaultValue="google" append=".com" />
-    </>
+    </div>
 )
 pandInput.story = {
-    name: '带前后缀的 Input'
+    name: '带前后缀的 Input',
+    parameters: {
+        docs: {
+            source: {
+                code: "import Input from './input' \n \n <Input defaultValue='prepend text' prepend='https//' /> \n <Input defaultValue='google' append='.com' />"
+            }
+        },
+        controls: { hideNoControlsWarning: true }
+    }
 }
